38aae7c9ac0eae87a8c4ee4473fc21393aef69a5
shota12085 <shota.toyoshima007@gmail.com>
2020-05-22 22:54:30 +0900

    文字アニメーション追加

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -52,5 +52,39 @@ $(function(){
     $('.about__introduction').removeClass('showIn');
   })
 
+  
+})
+$(function () {
+  $('.test').textillate({
+    selector: '.texts',	// 要素を指定
+    loop: false,	// ループ繰り返し
+    minDisplayTime: 3000,	// アニメーションの間隔時間
+    initialDelay: 100,	// アニメーション開始までの遅延時間
+    autoStart: true,	// アニメーションの自動スタート
+    inEffects: [],	// エフェクト開始時のアニメーション設定
+    outEffects: [ 'hinge' ],	// エフェクト終了時のアニメーション設定
+  
+    in: {
+      effect: 'fadeInLeftBig',	// エフェクトの指定
+      delayScale: 1.5,	// 遅延時間の指数
+      delay: 100,	// 文字ごとの遅延時間
+      sync: false,	// アニメーションをすべての文字に同時に適用
+      shuffle: false, // 文字のランダム表示
+      reverse: false,	// エフェクトを逆に再生（「sync:true」のときは不可）
+      callback: function () {}	// コールバック関数
+    },
+  
+    out: {
+      effect: 'hinge',  
+      delayScale: 1.5,  
+      delay: 50,  
+      sync: false,  
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    },
+    callback: function () {},
+    type: 'char'
+  });
 })
 
