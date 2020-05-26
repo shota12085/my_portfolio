a121a19eef9c29ff7b05a45f15dfd6df2c68ed78
shota12085 <shota.toyoshima007@gmail.com>
2020-05-27 01:43:13 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                  587,
+    port:                  25,
     domain:               'gmail.com',
     user_name:             ENV['EMAIL'],
     password:              ENV['SECRET_PASS'],
