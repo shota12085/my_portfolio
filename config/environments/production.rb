76708fe5f9f4b574d0af914202dbded80905c469
shota12085 <shota.toyoshima007@gmail.com>
2020-05-27 01:50:06 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'shota12085.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:             ENV['EMAIL'],
+    password:              ENV['SECRET_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -91,16 +103,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default_url_options = { :host => 'shota12085.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                  25,
-    domain:               'gmail.com',
-    user_name:             ENV['EMAIL'],
-    password:              ENV['SECRET_PASS'],
-    authentication:       'plain',
-    enable_starttls_auto:  true
-  }
+  
 end
