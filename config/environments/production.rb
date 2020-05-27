6cbcb920b579f26bc61bcc56c02e2f42c8d052a3
shota12085 <shota.toyoshima007@gmail.com>
2020-05-27 11:01:42 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,16 +64,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "my_portfolio_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { protocol: 'https',host: 'shota12085.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'shota12085.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
     user_name:             ENV['EMAIL'],
-    password:              ENV['PASS'],
     password:              ENV['SECRET_PASS'],
     authentication:       'plain',
     enable_starttls_auto:  true
