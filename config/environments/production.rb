07d7c868bfacb8425a9fd3aec6ff428025c2738e
shota12085 <shota.toyoshima007@gmail.com>
2020-05-27 09:44:05 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "my_portfolio_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'shota12085.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
